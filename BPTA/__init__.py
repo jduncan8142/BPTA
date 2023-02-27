a39d89e14bb9ca9d89b6f129043055617529dbc9
Jason Duncan <jason.matthew.duncan@gmail.com>
2023-02-26 19:35:49 -0600

    Complete rework 2023

--- BPTA/__init__.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC
-from Core.Base import BusinessProcess, Scenario, Task, Step, Action, Result, Status, Dict2Obj
-from Core.Logger import Logger
-from Core.Screenshot import Screenshot
-from Core.Timer import Timer
-from Core.WindowHandler import WindowHandler
-from Core import test2
